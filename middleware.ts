696a98fb285e4c4fbbc57cc14d06acfce5784c20
Luis Gomez <luis.gomez@kodevox.com>
2026-01-05 21:46:07 -0600

    Fix: corrección middleware

--- middleware.ts
+++ middleware.ts
@@ -1,4 +1,20 @@
-export { default } from "next-auth/middleware"
+// middleware.ts
+import { withAuth } from "next-auth/middleware"
+
+export default withAuth({
+  pages: {
+    signIn: "/login", // La página a la que redirige si no hay sesión
+  },
+})
+
+// Opcional: Define qué rutas proteger
+export const config = { 
+  matcher: [
+    "/((?!api|_next/static|_next/image|favicon.ico|login|registro).*)",
+  ] 
+}
+
+/*export { default } from "next-auth/middleware"
 
 // Rutas que quieres proteger
-export const config = { matcher: ["/", "/reportes", "/configuracion"] }
+export const config = { matcher: ["/", "/reportes", "/configuracion"] }*/
